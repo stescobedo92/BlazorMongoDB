35ac195956a393ee85d6b6b631506a15849158ec
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-05-29 17:25:31 -0600

    Added and implemented methods for product class

--- BlazorMongoDbClient/Services/IProductService.cs
+++ BlazorMongoDbClient/Services/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDbApi.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,9 @@ namespace BlazorMongoDbClient.Services
 {
     public interface IProductService
     {
+        Task<IEnumerable<Product>> GetAllProducts();
+        Task<Product> GetProductDetails(string id);
+        Task SaveProduct(Product product);
+        Task DeleteProduct(string id);
     }
 }
--- BlazorMongoDbClient/Services/ProductService.cs
+++ BlazorMongoDbClient/Services/ProductService.cs
@@ -1,11 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
+using MongoDbApi.Model;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace BlazorMongoDbClient.Services
 {
-    public class ProductService
+    public class ProductService : IProductService
     {
+        private readonly HttpClient _httpClient;
+
+        public Task DeleteProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductDetails(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
