41853ee7f7bbdd13e36fbc0ec2dce23430106d02
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-05-29 16:07:35 -0600

    Add project files.

---
+++ MongoDbApi/Controllers/ProductController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MongoDbApi.Models;
+using MongoDbApi.Repositories;
+
+namespace MongoDbApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private IProductCollection db = new ProductCollection();
+        
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            return Ok(await db.GetAllProducts());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductDetails(string id)
+        {
+            return Ok(await db.GetProductById(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] Product product)
+        {
+            if(product == null)
+            {
+                return BadRequest();
+            }
+
+            if(product.Name == string.Empty)
+            {
+                ModelState.AddModelError("Name", "The product shouldn't be empty");
+            }
+
+            await db.InsertProduct(product);
+
+            return Created("Created", true);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product,string id)
+        {
+            if (product == null)
+            {
+                return BadRequest();
+            }
+
+            if (product.Name == string.Empty)
+            {
+                ModelState.AddModelError("Name", "The product shouldn't be empty");
+            }
+
+            product.Id = new MongoDB.Bson.ObjectId(id);
+            await db.UpdateProduct(product);
+
+            return Created("Created", true);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            await db.DeleteProduct(id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ MongoDbApi/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDbApi.Models
+{
+    public class Product
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public string Category { get; set; }
+    }
+}
